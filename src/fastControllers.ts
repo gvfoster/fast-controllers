19f5169a6cffbd427e92c7407112f4cb489ac99a
Greg Foster <greg@georgia.com>
2022-10-21 10:45:38 -0400

    0.0.2

--- src/fastControllers.ts
+++ src/fastControllers.ts
@@ -1,13 +1,13 @@
-/* eslint-disable indent */
 import path from 'node:path'
 import fs from 'node:fs'
 
 import type { FastifyInstance, FastifyPluginOptions, RouteOptions } from 'fastify'
- 
+
 import type FastController from './FastController'
 import FastControllerError_InvalidControllerPath from './errors/FastControllerError_InvalidControllerPath'
 
-type FastControllerModule = {controller: typeof FastController, route: string}
+
+type FastControllerModule = { controller: typeof FastController, route: string }
 
 /**
  * FastControllers Fastify Plugin
@@ -22,35 +22,35 @@ type FastControllerModule = {controller: typeof FastController, route: string}
  * @param options  - The provided Fastify options object
  * @param done     - The plugin done callback 
  */
-async function fastControllers( instance: FastifyInstance, options: FastifyPluginOptions & {path: string}, done: (err?:Error) => void ) {
+async function fastControllers(instance: FastifyInstance, options: FastifyPluginOptions & { path: string }, done: (err?: Error) => void) {
 
-    if( !options || !options.path || !path.isAbsolute(options.path) ) {
+    if (!options || !options.path || !path.isAbsolute(options.path)) {
         throw new FastControllerError_InvalidControllerPath(options.path || 'undefined')
     }
 
-    new Promise<Array<string>>( (resolve, reject) => {
+    new Promise<Array<string>>((resolve, reject) => {
 
         function scan(path: string): Array<string> {
-        
-            const paths = new Array<string>() 
+
+            const paths = new Array<string>()
             const dir = fs.opendirSync(path)
-        
+
             let entry = dir.readSync()
-            while(entry) {
-                
-                if(entry.isDirectory() && !entry.name.startsWith('.')) {
-                    paths.push( ...scan(dir.path + '/' + entry.name) )
+            while (entry) {
+
+                if (entry.isDirectory() && !entry.name.startsWith('.')) {
+                    paths.push(...scan(dir.path + '/' + entry.name))
                 }
-    
-                else if(entry.isFile() && !entry.name.startsWith('index', 0) ) {
-    
+
+                else if (entry.isFile() && !entry.name.startsWith('index', 0)) {
+
                     let modulePath = (dir.path + '/' + entry.name)
-                    paths.push( modulePath.substring(0, modulePath.lastIndexOf('.')) )
+                    paths.push(modulePath.substring(0, modulePath.lastIndexOf('.')))
                 }
-                
+
                 entry = dir.readSync()
             }
-    
+
             dir.close()
             return paths
         }
@@ -58,54 +58,54 @@ async function fastControllers( instance: FastifyInstance, options: FastifyPlugi
         resolve(scan(options.path))
     })
 
-    // Then import each module and return an array of modules with routes  
-    .then( paths => { 
-      
-        return Promise.all( paths.map( path => {
-            return import(path).then( module => {
-                return {
-                    controller: module.default, 
-                    route: path.substring( options.path.length ).toLowerCase()
-                } as FastControllerModule
-            }) 
-        }))
-    })        
-        
-    // Split the modules array into an array of FastControllerModules indexed by scope 
-    .then( modules => {
+        // Then import each module and return an array of modules with routes  
+        .then(paths => {
 
-        const scopedModules: { [index:string]: Array<FastControllerModule> } = {}
-
-        modules.forEach( module => {
-            if(!scopedModules[module.controller.scope]) 
-                scopedModules[module.controller.scope] = new Array<FastControllerModule>()
-            
-            scopedModules[module.controller.scope]?.push(module)
-        })
- 
-        return scopedModules
-    })
-
-    // Return a consolidated promise for all controller scopes
-    .then( scopedModules => {
-
-        return Promise.all( Object.keys(scopedModules).map( scope => {
-            return instance.register( ( _scope, options, next) => {
-                scopedModules[scope]?.forEach( module => {
-                    _scope.route(new module.controller(_scope, module.route) as RouteOptions)
+            return Promise.all(paths.map(path => {
+                return import(path).then(module => {
+                    return {
+                        controller: module.default,
+                        route: path.substring(options.path.length).toLowerCase()
+                    } as FastControllerModule
                 })
-                
-                next()
+            }))
+        })
+
+        // Split the modules array into an array of FastControllerModules indexed by scope 
+        .then(modules => {
+
+            const scopedModules: { [index: string]: Array<FastControllerModule> } = {}
+
+            modules.forEach(module => {
+                if (!scopedModules[module.controller.scope])
+                    scopedModules[module.controller.scope] = new Array<FastControllerModule>()
+
+                scopedModules[module.controller.scope]?.push(module)
             })
-        }))
-    })
 
-    .catch( err => {
-        console.error(err)
-        done(err)
-    })
+            return scopedModules
+        })
 
-    .then( () => { done() } )
+        // Return a consolidated promise for all controller scopes
+        .then(scopedModules => {
+
+            return Promise.all(Object.keys(scopedModules).map(scope => {
+                return instance.register((_scope, options, next) => {
+                    scopedModules[scope]?.forEach(module => {
+                        _scope.route(new module.controller(_scope, module.route) as RouteOptions)
+                    })
+
+                    next()
+                })
+            }))
+        })
+
+        .catch(err => {
+            console.error(err)
+            done(err)
+        })
+
+        .then(() => { done() })
 }
 
 export default fastControllers
